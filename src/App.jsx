4fd10cd6d7f2dfa6b07c8c8df4191351f6b3474d
yashwanth535 <yashwanth.lumia535@gmail.com>
2025-02-24 16:12:17 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-const App = () => {
+function App() {
+  const [users, setUsers] = useState([]);
+  const API_URL = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    fetch(`${API_URL}/api/users`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="text-4xl text-red-200 font-bold">
-    Hello word!
-  </div>
-  )
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🚀 User List</h1>
+      <div className="text-4xl text-red-200 font-bold">
+        Hello word!
+      </div>
+      {users.length > 0 ? (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.age} years old
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+    
+  );
 }
-export default App
+
+export default App;
